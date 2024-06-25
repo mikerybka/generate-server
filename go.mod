5812fdad53555048893fdebb2458ffbf0dc4e2d3
Mike Rybka <merybka@gmail.com>
2024-06-25 14:34:43 -0400

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikerybka/generate-server
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Pull schemas from schema.cafe
+
+	// Generate types package
+
+	// Generate main.go
+}
